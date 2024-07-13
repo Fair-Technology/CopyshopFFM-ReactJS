7727a1b8098b390a7112faacd46a3351def86018
Manish Ghimire <manishghimire@hotmail.com>
2024-07-13 14:07:50 +1000

    COP-10:  Implemented i18n for multi-language support

---
+++ src/services/i18n/i18n-config.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import enTranslation from "./locales/en/translation.json";
+import deTranslation from "./locales/de/translation.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+    de: {
+      translation: deTranslation,
+    },
+  },
+  lng: "de", // default language
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
