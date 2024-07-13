0fb91d6c2a4ae5ff7d3c2d9e9b85020561be4f48
Runqing-Programer <maorunqing1997@gmail.com>
2024-07-13 12:05:39 +1000

    COP5: Make Nav Responsive and detail adjustments
    Now the nav will cover the screen.
    the item list will flow to next line when screen shrink below small size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,32 +2,34 @@ import logoImg from '../assets/Copyshop-FFM-Logo_img.png'
 
 const Navbar = () => {
   return (
-    <div className="container bg-red-600 mx-auto flex justify-between items-center h-20">
-      <Logo />
-      <NavList />
+  <div className=" bg-red-600 mx-auto w-full p-4">
+    <div className="flex-warp sm:flex justify-between p-4">
+      <div className="flex items-center justify-center  min-w-36 min-h-18 sm:mb-0">
+        <Logo />
+      </div>
+      <div className="flex items-center justify-center min-w-48 min-h-18">
+        <NavList />
+      </div>
     </div>
-   
+  </div>
   )
 }
 
 export default Navbar
 
 const Logo = () => { 
-  return <div className="pl-8 w-64 h-24 ">
-    <img className="w-64 h-24 " src={logoImg} alt="Logo" />
-  </div>
-  
+  return <img className="w-64 h-24 " src={logoImg} alt="Logo" />
  }
 
 const NavList = () => { 
   return (
-    <ul className="flex gap-4 pr-8 text-lg text-white">
+    <ul className="flex gap-8 px-8 text-2xl text-white">
     <NavListItem page ='Home' />
-    <NavListItem page ='Cart'/>
+    <NavListItem page ='Cart' />
     <NavListItem page ='OrderState'/>
     </ul>
   ) 
 }
- const NavListItem = ({page}:{page:string}) =>{
-  return (<li>{page}</li>)
+const NavListItem = ({page, buttonClass}:{page:string, buttonClass?:string}) =>{
+  return <li><button className={buttonClass}>{page}</button></li>
 }
