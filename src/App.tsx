b0803a2c8867652901f9ac83b5063f06d97f24af
Runqing-Programer <maorunqing1997@gmail.com>
2024-07-14 17:00:24 +1000

    COP-12-Button added to the Calulation. Navbar renamed and import adjisted in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import ContentArea from "./components/contentArea";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="app container mx-auto">
-      <ContentArea />
-    </div>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className="app container mx-auto">
+				<ContentArea />
+			</div>
+		</>
+	);
 }
 
 export default App;
-
-
--- src/components/calculationArea.tsx
+++ src/components/calculationArea.tsx
@@ -1,74 +1,88 @@
 import Heading from "./heading";
 
 const CalculationArea = () => {
-  return <div>
-    <FormInfoList />
-    
-    <div className="pt-3">
-      <Heading text="Total" />
-      <CostSum />
-    </div>
-    
-  </div>;
+	return (
+		<div>
+			<FormInfoList />
+
+			<div className="pt-3">
+				<Heading text="Total" />
+				<CostSum />
+				<div className="mt-4">
+					<AddToCartBtn />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CalculationArea;
 
 const FormInfoList = () => {
-  return <div className="py-2"> 
-    <table className="min-w-full bg-white border border-gray-200">
-      <tbody>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Paper Format</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">A4</td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Paper weight</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">80g/m<sup>2</sup></td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Print Side</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">One-Sided</td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Pages in PDF</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">
-            <div>Black and white: 3</div>
-            <div>Color: 2</div>
-          </td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Copies</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">3 copies</td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Next Step</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">Print</td>
-        </tr>
-        <tr>
-          <td className="py-2 px-4 border-b border-gray-200">Options</td>
-          <td className="py-2 px-4 border-b border-l border-gray-200">NA</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+	return (
+		<div className="py-2">
+			<table className="min-w-full bg-white border border-gray-200">
+				<tbody>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Paper Format</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">A4</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Paper weight</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">
+							80g/m<sup>2</sup>
+						</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Print Side</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">
+							One-Sided
+						</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Pages in PDF</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">
+							<div>Black and white: 3</div>
+							<div>Color: 2</div>
+						</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Copies</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">
+							3 copies
+						</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Next Step</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">
+							Print
+						</td>
+					</tr>
+					<tr>
+						<td className="py-2 px-4 border-b border-gray-200">Options</td>
+						<td className="py-2 px-4 border-b border-l border-gray-200">NA</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
 
- }
-
- const CostSum =()=>{
-  return <div>
-    <div className="min-w-full bg-blue-100">
-      <div className="flex justify-between gap-6">
-        <div className="py-2 pl-1">Products Total Cost</div>
-        <div className="py-2 pr-1">&euro; 0.00</div>
-      </div>   
-    </div>
-    <div className="min-w-full">
-      <div className="flex justify-between gap-6">
-        <div className="py-2 pl-1">DHL Delivery Cost</div>
-        <div className="py-2 pr-1">&euro; 0.00 </div>
-      </div>
-      {/* <div className="flex justify-between gap-6">
+const CostSum = () => {
+	return (
+		<div>
+			<div className="min-w-full bg-blue-100">
+				<div className="flex justify-between gap-6">
+					<div className="py-2 pl-1">Products Total Cost</div>
+					<div className="py-2 pr-1">&euro; 0.00</div>
+				</div>
+			</div>
+			<div className="min-w-full">
+				<div className="flex justify-between gap-6">
+					<div className="py-2 pl-1">DHL Delivery Cost</div>
+					<div className="py-2 pr-1">&euro; 0.00 </div>
+				</div>
+				{/* <div className="flex justify-between gap-6">
         <div>
         DHL Delivery Fee:
         </div>
@@ -76,7 +90,26 @@ const FormInfoList = () => {
         &euro; 0.00
         </div>
       </div> */}
-      <div className="bg-orange-500 text-sm px-1">* If you pick at store, the delivery fee will be 0</div>
-    </div>
-  </div>
- }
+				<div className=" text-sm px-1">
+					* If you pick at store, the delivery fee will be &euro;0.
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const AddToCartBtn = () => {
+	return (
+		<div
+			onClick={AddToCart}
+			className="bg-red-600 text-white text-xl font-bold grid place-content-center min-h-12"
+		>
+			Add to Cart
+		</div>
+	);
+};
+
+const AddToCart = () => {
+	alert("You successfully added the items into Cart");
+	return;
+};
