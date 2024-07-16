3a2a5bcf368ae557f34b1027bb92273aca981620
Runqing-Programer <maorunqing1997@gmail.com>
2024-07-16 21:49:57 +1000

    COP-4: frame created

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,5 +1,20 @@
 const CheckOutPage = () => {
-	return <div>CheckOutPage</div>;
+	return (
+		<div className="mx-auto sm:flex justify-center bg-cyan-700">
+			<div className="sm:w-2/3 bg-black text-white text-center">
+				<section className="bg-red-500 border-8 border-zinc-500">
+					home address
+				</section>
+				<section className="bg-green-300">a toggle buttion</section>
+				<section className=" border-8 border-zinc-500">
+					shipping address
+				</section>
+			</div>
+			<div className="sm:w-1/3 bg-green-400 text-white text-center">
+				Right Container
+			</div>
+		</div>
+	);
 };
 
 export default CheckOutPage;
