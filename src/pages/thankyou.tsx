e1a53a66225fc809a4f40ca123204224c185ae02
Runqing-Programer <maorunqing1997@gmail.com>
2024-07-25 16:19:31 +1000

    COP7-Upper part updated

--- src/pages/thankyou.tsx
+++ src/pages/thankyou.tsx
@@ -1,8 +1,11 @@
+import PayPal from "../components/paypal.tsx";
+import { FaRegCheckCircle } from "react-icons/fa";
+
 const ThankYouPage = () => {
 	return (
-		<div>
+		<div className="bg-slate-100 pt-4">
 			<ThankYou />
-			<PayPal />
+			<PayPalDiv />
 			<DetailCards />
 		</div>
 	);
@@ -12,23 +15,48 @@ export default ThankYouPage;
 
 const ThankYou = () => {
 	return (
-		<div>
-			<div className="bg-red-100 flex justify-center">Thank you</div>
-			<div className="bg-red-200 flex justify-center">
-				<OrderNumTag />
+		<div className=" pb-4">
+			<div className="flex  flex-col justify-center ">
+				<div className="flex justify-center">
+					<FaRegCheckCircle size={"100"} color="#93c5fd" />
+				</div>
+				<div className="text-6xl text-center text-blue-300">Thank You</div>
 			</div>
-			<div className="bg-red-300 flex justify-center">StateIcons</div>
+			<div className="flex justify-center py-2">
+				<OrderNumTag orderNumber="#123456" bgcolor="bg-blue-300" />
+			</div>
+			<div className="bg-red-300 flex justify-center">
+				<StateIcons />
+			</div>
 		</div>
 	);
 };
 
-const OrderNumTag = () => {
-	return <div>OrderTag</div>;
+const OrderNumTag = ({
+	orderNumber,
+	bgcolor,
+}: {
+	orderNumber: string;
+	bgcolor: string;
+}) => {
+	return (
+		<div className={bgcolor + " text-2xl py-1 px-2 rounded-lg"}>
+			Order Number: {orderNumber}
+		</div>
+	);
+};
+const StateIcons = () => {
+	return <div>4 ICONS: Order Payment Editing Sent</div>;
 };
 
-const PayPal = () => {
-	return <div>PayPal</div>;
+const PayPalDiv = () => {
+	return (
+		<div className="flex justify-center">
+			<PayPal />
+		</div>
+	);
 };
+
 const DetailCards = () => {
 	return <div>DetailCards</div>;
 };
